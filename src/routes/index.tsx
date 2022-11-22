b8f7c2b66626c1e9f405124e9d8671fe7031dae1
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-22 00:14:41 -0300

    Inclusão de texto

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {View} from 'react-native';
+import {View, ActivityIndicator} from 'react-native';
 
 import AppRoutes from './app.routes';
 import AuthRoutes from './auth.routes';
@@ -8,6 +8,21 @@ import AuthRoutes from './auth.routes';
 function Routes(){
   const isAuthenticated = false;
   const loading = false;
+
+  if(loading){
+    return(
+      <View 
+      style={{ 
+        flex:1, 
+        backgroundColor: '#f5f7fb', 
+        justifyContent: 'center', 
+        alignItems:'center' 
+        }}
+      >
+        <ActivityIndicator />
+      </View>
+    )
+  }
 
 
   return(
