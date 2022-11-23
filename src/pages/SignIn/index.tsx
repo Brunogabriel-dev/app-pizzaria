7f079602a38a1333205f29db9969328398ac749b
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-11-22 22:06:23 -0300

    Inclusão de texto

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -22,18 +22,22 @@ export default function SignIn(){
           style={styles.input}
           placeholderTextColor="#f0f0f0"
         />
+
         <TextInput
           placeholder="Sua senha"
           style={styles.input}
           placeholderTextColor="#f0f0f0"
         />
 
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+
       </View>
 
     </View>
   )
 }
-
 const styles = StyleSheet.create({
   container:{
     flex:1,
@@ -59,6 +63,18 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     paddingHorizontal: 8,
     color: '#fff'
-
+  },
+  button:{
+    width: '95',
+    height: 40,
+    backgroundColor: '#3fffa3',
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#101026'
   }
 })
